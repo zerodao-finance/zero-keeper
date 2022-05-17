f1cf1e6750a10f909ac43755179325e1c1f0f31c
UndercovercameL <undercover@camel>
2022-05-17 05:11:13 +0000

    chore: basic dispatcher stuff

--- bin/dispatcher.js
+++ bin/dispatcher.js
@@ -1,26 +1,68 @@
-const redis = require('ioredis')(process.env.REDIS_URI)
-import _ from 'lodash'
+const redis = require("ioredis")(process.env.REDIS_URI);
+const ethers = require("ethers");
+const {
+  UnderwriterTransferRequest,
+  UnderwriterBurnRequest,
+} = require("zero-protocol");
+
+const requestTypes = {
+  transfer: {
+    underlyingClass: UnderwriterTransferRequest,
+    funcs: ["loan", "repay"],
+  },
+  burn: {
+    underlyingClass: UnderwriterBurnRequest,
+    funcs: ["burn"],
+  },
+};
 
 /**
  * @static wait { Promise }
- * @returns TransferRequest | BurnRequest | MetaRequest 
+ * @returns TransferRequest | BurnRequest | MetaRequest
+ * fetches request from dispatch queue
+ *
  */
 class Dispatcher {
-    static create(){ 
-        return new Dispatcher()
+  constructor({ rpcUrl, pvtKey, preset }) {
+    this.provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+    this.signer = new ethers.Wallet(pvtKey, provider);
+    this.preset = preset;
+  }
+  formatRequest(_r) {
+    const requestType = requestTypes[_r.requestType];
+    const request = new requestType.underlyingClass(_r);
+    request.funcs = requestType.funcs;
+    if (preset == "badger") {
+      request.dry = async () => [];
+      request.loan = async () => ({
+        async wait() {
+          return {};
+        },
+      });
     }
-
-    static wait(){ 
-        Promise((resolve, reject) => {
-            while (true) {
-                if (redis.llen('/zero/request') !== 0) break;
-            }
-            console.log(`received message from keeper`)
-            resolve(redis.lpop('/zero/request'))
-        })
+    return request;
+  }
+  async wait() {
+    const [_request] = await redis.brpop("/zero/dispatch");
+    let request;
+    try {
+      request = JSON.parse(_request);
+    } catch (e) {
+      console.error("Error parsing request");
     }
-    
-    constructor(){
-        
-    }
+    return this.formatRequest(request);
+  }
+  async execute(request) {
+    await request.funcs.reduce(async (_r, func) => {
+      await _r;
+      try {
+        console.log("Executing:", func);
+        const tx = await request[func](this.signer);
+        const receipt = await tx.wait();
+        console.log(func, "receipt: ", receipt);
+      } catch (e) {
+        console.error("Error on tx:", e);
+      }
+    }, Promise.resolve());
+  }
 }
